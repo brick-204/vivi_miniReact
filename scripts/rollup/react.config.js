1e6744f51d00521d8d86cecfb65821b7c618bf57
caicai <xxx.qq.com>
2026-03-18 18:40:28 +0800

    “feat:  jsx 的实现和rollup打包”

---
+++ scripts/rollup/react.config.js
@@ -0,0 +1,55 @@
+import {
+	getPackageJson,
+	resolvePkgPath,
+	getBaseRollupPlugins
+} from './utils.js';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+
+const { name, module } = getPackageJson('react');
+// 获取react包的路径
+const pkgPath = resolvePkgPath(name);
+// 获取react包的dist路径
+const pkgDistPath = resolvePkgPath(name, true);
+
+export default [
+	// react
+	{
+		input: `${pkgPath}/${module}`,
+		output: {
+			file: `${pkgDistPath}/index.js`,
+			name: 'index.js',
+			format: 'umd'
+		},
+		plugins: [
+			...getBaseRollupPlugins(),
+			generatePackageJson({
+				inputFolder: pkgPath,
+				outputFolder: pkgDistPath,
+				baseContents: ({ name, version, description }) => ({
+					name,
+					version,
+					description,
+          main:'index.js'
+				})
+			})
+		]
+	},
+	// jsx-runtime
+	{
+		input: `${pkgPath}/src/jsx.ts`,
+		output: [
+			{
+				file: `${pkgDistPath}/jsx-runtime.js`,
+				name: 'jsx-runtime.js',
+				format: 'umd'
+			},
+			// jsx-dev-runtime
+			{
+				file: `${pkgDistPath}/jsx-dev-runtime.js`,
+				name: 'jsx-dev-runtime.js',
+				format: 'umd'
+			}
+		],
+		plugins: getBaseRollupPlugins()
+	}
+];

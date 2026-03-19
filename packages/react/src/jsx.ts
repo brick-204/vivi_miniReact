fa94c5d57ada609009893df54b07f64d41eea9f8
caicai <xxx.qq.com>
2026-03-19 10:23:32 +0800

    refactor(react): 替换jsx为jsxDEV实现并优化属性处理

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -62,6 +62,29 @@ export const jsx = (type: ElementType, config: Props, ...maybeChildren: unknown[
   return ReactElement(type, key, ref, props);
 };
 
-// 生产环境和开发环境一致
-export const jsxDev = jsx;
-export const jsxDEV = jsx;
+export const jsxDEV = (type: ElementType, config: Props) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+  // 处理config中的key和ref, 其他属性注入 props 中
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val as Ref;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+  return ReactElement(type, key, ref, props);
+};

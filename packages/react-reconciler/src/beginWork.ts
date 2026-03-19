5105839c9d4ed390bbd874e44db47e36a2be6b6d
caicai <xxx.qq.com>
2026-03-19 19:30:29 +0800

    feat(updateQueue): 实现更新队列的基本功能
    添加 createUpdate、createUpdateQueue、enqueueUpdate 和 processUpdateQueue 方法
    实现更新队列的创建、更新入队和状态处理逻辑

--- packages/react-reconciler/src/beginWork.ts
+++ packages/react-reconciler/src/beginWork.ts
@@ -4,5 +4,4 @@ import { FiberNode } from './fiber';
 // DFS 遍历 递归中的递阶段
 export const beginWork = (fiber: FiberNode) => {
   // 比较，返回子fiberNode
-  
 }
--- packages/react-reconciler/src/updateQueue.ts
+++ packages/react-reconciler/src/updateQueue.ts
@@ -1,5 +1,52 @@
 import { Action } from 'shared/ReactTypes';
 
 export interface Update<State> {
-  action: Action<State>;
+	action: Action<State>;
 }
+
+export interface UpdateQueue<State> {
+	shared: {
+		pending: Update<State> | null;
+	};
+}
+
+export const createUpdate = <State>(action: Action<State>): Update<State> => {
+	return {
+		action
+	};
+};
+
+export const createUpdateQueue = <Action>() => {
+	return {
+		shared: {
+			pending: null
+		}
+	} as UpdateQueue<Action>;
+};
+
+export const enqueueUpdate = <Action>(
+	UpdateQueue: UpdateQueue<Action>,
+	update: Update<Action>
+) => {
+	UpdateQueue.shared.pending = update;
+};
+
+export const processUpdateQueue = <State>(
+	baseState: State,
+	pendingUpdate: Update<State>
+): { memorizedState: State } => {
+	const result: ReturnType<typeof processUpdateQueue<State>> = {
+		memorizedState: baseState
+	};
+	if (pendingUpdate !== null) {
+		const action = pendingUpdate.action;
+		if (action instanceof Function) {
+			// 处理函数类型的 action  setState((prevState) => prevState + 1)
+			result.memorizedState = action(baseState);
+		} else {
+			// 处理普通类型的 action  setState(100)
+			result.memorizedState = action;
+		}
+	}
+	return result;
+};
